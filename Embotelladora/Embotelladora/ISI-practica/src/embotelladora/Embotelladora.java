607807aecafd912496536a00f9f511bc6dcdb03e
Juan Antonio <ja.ortega.2017@alumnos.urjc.es>
2021-12-02 18:47:11 +0100

    Creada ramaEmbotelladora y añadido el proyecto de la embotelladora con sus tests

---
+++ Embotelladora/Embotelladora/ISI-practica/src/embotelladora/Embotelladora.java
@@ -0,0 +1,20 @@
+package embotelladora;
+
+public class Embotelladora {
+	
+	public Embotelladora() {
+		
+	}
+	
+	// @param pequenas: número de botellas en almacén de 1L
+	// grandes : n´umero de botellas en almacén de 5L
+	// total : n´umero total de litros que hay que embotellar
+	// @return n´umero de botellas peque~nas necesarias para embotellar el total de l´ıquido, teniendo
+	// en cuenta que hay que minimizar el n´umero de botellas peque~nas: primero
+	// se rellenan las grandes
+	// @throws NoSolution si no es posible embotellar todo el l´ıquido
+	public int calculaBotellasPequenas(int pequenas, int grandes, int total) throws NoSolution {
+	//
+		return 0;
+	}
+}
