cc4a0332dd3e754606eab5f6357a9f6b21abf2c5
César Borao <cesarboraom@gmail.com>
2021-12-04 14:00:05 +0100

    Añadidos tests 26 y 27 de números romanos y modificado la errrata del 'convierte', close #28

--- RomanNumeral/src/RomanNumeralTest.java
+++ RomanNumeral/src/RomanNumeralTest.java
@@ -10,77 +10,77 @@ public class RomanNumeralTest {
 	   @Test public void OrderedNonRepeatedSymbols() 
 	   {
 	      String s = "MDCLXVI";
-	      assertEquals("Nonzero in first element", 1666, RomanNumeral.romanNumeral(s));
+	      assertEquals("Nonzero in first element", 1666, RomanNumeral.convierte(s));
 	   }
 	   
 	   // Test 2
 	   @Test public void NonOrderedNonRepeatedSymbols_1() 
 	   {
 	      String s = "XIV";
-	      assertEquals("Nonzero in first element", 14, RomanNumeral.romanNumeral(s));
+	      assertEquals("Nonzero in first element", 14, RomanNumeral.convierte(s));
 	   }
 
 	   // Test 3
 	   @Test public void NonOrderedNonRepeatedSymbols_2() 
 	   {
 		   String s = "IX";
-		   assertEquals("Nonzero in first element", 9, RomanNumeral.romanNumeral(s));
+		   assertEquals("Nonzero in first element", 9, RomanNumeral.convierte(s));
 	   }
 	   
 	   // Test 4
 	   @Test public void NonOrderedNonRepeatedSymbols_3() 
 	   {
 		   String s = "XLI";
-		   assertEquals("Nonzero in first element", 41, RomanNumeral.romanNumeral(s));
+		   assertEquals("Nonzero in first element", 41, RomanNumeral.convierte(s));
 	   }
 	   
 	   // Test 5
 	   @Test public void NonOrderedNonRepeatedSymbols_4() 
 	   {
 		   String s = "CM";
-		   assertEquals("Nonzero in first element", 900, RomanNumeral.romanNumeral(s));
+		   assertEquals("Nonzero in first element", 900, RomanNumeral.convierte(s));
 	   }
 	   
 	   // Test 6
 	   @Test public void NonOrderedNonRepeatedSymbols_5() 
 	   {
 		   String s = "CDI";
-		   assertEquals("Nonzero in first element", 401, RomanNumeral.romanNumeral(s));
+		   assertEquals("Nonzero in first element", 401, RomanNumeral.convierte(s));
 	   }
 	   
 	   // Test 7
 	   @Test public void NonOrderedRepeatedSymbols_1() 
 	   {
 		   String s = "XXIV";
-		   assertEquals("Nonzero in first element", 24, RomanNumeral.romanNumeral(s));
+		   assertEquals("Nonzero in first element", 24, RomanNumeral.convierte(s));
 	   }
 	   
 	   // Test 8
 	   @Test public void NonOrderedRepeatedSymbols_2() 
 	   {
 		   String s = "XIX";
-		   assertEquals("Nonzero in first element", 19, RomanNumeral.romanNumeral(s));
+		   assertEquals("Nonzero in first element", 19, RomanNumeral.convierte(s));
 	   }
 	   
 	   // Test 9
 	   @Test public void NonOrderedRepeatedSymbols_3() 
 	   {
 		   String s = "CDIV";
-		   assertEquals("Nonzero in first element", 404, RomanNumeral.romanNumeral(s));
+		   assertEquals("Nonzero in first element", 404, RomanNumeral.convierte(s));
 	   }
 	   
 	   // Test 10
 	   @Test public void NonOrderedRepeatedSymbols_4() 
 	   {
 		   String s = "DCXXV";
-		   assertEquals("Nonzero in first element", 625, RomanNumeral.romanNumeral(s));
+		   assertEquals("Nonzero in first element", 625, RomanNumeral.convierte(s));
 	   }
 	   
 	   // Test 11
 	   @Test public void NonOrderedRepeatedSymbols_5() 
 	   {
 		   String s = "MCM";
-		   assertEquals("Nonzero in first element", 1900, RomanNumeral.romanNumeral(s));
+		   assertEquals("Nonzero in first element", 1900, RomanNumeral.convierte(s));
 	   }
 	   
 	   // Test 12
@@ -89,7 +89,7 @@ public class RomanNumeralTest {
 		   String s = "IIII";
 		   
 		   try {
-			   assertEquals(4, RomanNumeral.romanNumeral(s));
+			   assertEquals(4, RomanNumeral.convierte(s));
 			   fail("Roman number rules broken, expected Exception");
 		   } catch (Exception IllegalArgumentException) {}
 	   }
@@ -100,7 +100,7 @@ public class RomanNumeralTest {
 		   String s = "XXXX";
 		   
 		   try {
-			   assertEquals("Nonzero in first element", 40, RomanNumeral.romanNumeral(s));
+			   assertEquals("Nonzero in first element", 40, RomanNumeral.convierte(s));
 			   fail("Roman number rules broken, expected Exception");
 		   } catch (Exception IllegalArgumentException) {}
 	   }
@@ -111,7 +111,7 @@ public class RomanNumeralTest {
 		   String s = "CCCCXXI";
 		   
 		   try {
-			   assertEquals(421, RomanNumeral.romanNumeral(s));
+			   assertEquals(421, RomanNumeral.convierte(s));
 			   fail("Roman number rules broken, expected Exception");
 		   } catch (Exception IllegalArgumentException) {}
 	   }
@@ -122,7 +122,7 @@ public class RomanNumeralTest {
 		   String s = "CMDC";
 		   
 		   try {
-			   assertEquals(1500, RomanNumeral.romanNumeral(s));
+			   assertEquals(1500, RomanNumeral.convierte(s));
 			   fail("Roman number rules broken, expected Exception");
 		   } catch (Exception IllegalArgumentException) {}
 	   }
@@ -132,7 +132,7 @@ public class RomanNumeralTest {
 	   {
 		   String s = "VVII";
 		   try {
-			   assertEquals(12, RomanNumeral.romanNumeral(s));
+			   assertEquals(12, RomanNumeral.convierte(s));
 			   fail("Roman number rules broken, expected Exception");
 		   } catch (Exception IllegalArgumentException) {}
 	   }
@@ -142,7 +142,7 @@ public class RomanNumeralTest {
 	   {
 		   String s = "LLV";
 		   try {
-			   assertEquals(105, RomanNumeral.romanNumeral(s));
+			   assertEquals(105, RomanNumeral.convierte(s));
 			   fail("Roman number rules broken, expected Exception");
 		   } catch (Exception IllegalArgumentException) {}
 	   }
@@ -152,7 +152,7 @@ public class RomanNumeralTest {
 	   {
 		   String s = "DDDIII";
 		   try {
-			   assertEquals(1503, RomanNumeral.romanNumeral(s));
+			   assertEquals(1503, RomanNumeral.convierte(s));
 			   fail("Roman number rules broken, expected Exception");
 		   } catch (Exception IllegalArgumentException) {}
 	   }
@@ -162,7 +162,7 @@ public class RomanNumeralTest {
 	   {
 		   String s = "IM";
 		   try {
-			   assertEquals(999, RomanNumeral.romanNumeral(s));
+			   assertEquals(999, RomanNumeral.convierte(s));
 			   fail("Roman number rules broken, expected Exception");
 		   } catch (Exception IllegalArgumentException) {}
 	   }
@@ -172,7 +172,7 @@ public class RomanNumeralTest {
 	   {
 		   String s = "XD";
 		   try {
-			   assertEquals(490, RomanNumeral.romanNumeral(s));
+			   assertEquals(490, RomanNumeral.convierte(s));
 			   fail("Roman number rules broken, expected Exception");
 		   } catch (Exception IllegalArgumentException) {}
 	   }
@@ -182,7 +182,7 @@ public class RomanNumeralTest {
 	   {
 		   String s = "LC";
 		   try {
-			   assertEquals(12, RomanNumeral.romanNumeral(s));
+			   assertEquals(12, RomanNumeral.convierte(s));
 			   fail("Roman number rules broken, expected Exception");
 		   } catch (Exception IllegalArgumentException) {}
 	   }
@@ -192,7 +192,7 @@ public class RomanNumeralTest {
 	   {
 		   String s = "VD";
 		   try {
-			   assertEquals(495, RomanNumeral.romanNumeral(s));
+			   assertEquals(495, RomanNumeral.convierte(s));
 			   fail("Roman number rules broken, expected Exception");
 		   } catch (Exception IllegalArgumentException) {}
 	   }
@@ -202,7 +202,7 @@ public class RomanNumeralTest {
 	   {
 		   String s = "LXL";
 		   try {
-			   assertEquals(90, RomanNumeral.romanNumeral(s));
+			   assertEquals(90, RomanNumeral.convierte(s));
 			   fail("Roman number rules broken, expected Exception");
 		   } catch (Exception IllegalArgumentException) {}
 	   }
@@ -212,7 +212,7 @@ public class RomanNumeralTest {
 	   {
 		   String s = "CCM";
 		   try {
-			   assertEquals(1000, RomanNumeral.romanNumeral(s));
+			   assertEquals(1000, RomanNumeral.convierte(s));
 			   fail("Roman number rules broken, expected Exception");
 		   } catch (Exception IllegalArgumentException) {}
 	   }
@@ -222,8 +222,28 @@ public class RomanNumeralTest {
 	   {
 		   String s = "XCD";
 		   try {
-			   assertEquals(410, RomanNumeral.romanNumeral(s));
+			   assertEquals(410, RomanNumeral.convierte(s));
 			   fail("Roman number rules broken, expected Exception");
+		   } catch (Exception IllegalArgumentException) {}
+	   }
+	   
+	   // Test 26
+	   @Test public void IncorrectSymbols_1() 
+	   {
+		   String s = "AUYB";
+		   try {
+			   assertNotEquals(700, RomanNumeral.convierte(s));
+			   fail("Roman Number does not exists, expected Exception");
+		   } catch (Exception IllegalArgumentException) {}
+	   }
+	   
+	   // Test 27
+	   @Test public void IncorrectSymbols_2() 
+	   {
+		   String s = "VOP";
+		   try {
+			   assertNotEquals(800, RomanNumeral.convierte(s));
+			   fail("Roman Number does not exists, expected Exception");
 		   } catch (Exception IllegalArgumentException) {}
 	   }
 }
