a4a3d66a388ba2036960985de97988eafaa6f64a
Pablo Barquero <p.barquero.2017@alumnos.urjc.es>
2021-12-06 18:11:49 +0100

    Modificando Tests BlackFriday

--- BlackFriday/BlackFriday/BlackFridayTest.java
+++ BlackFriday/BlackFriday/BlackFridayTest.java
@@ -15,7 +15,7 @@ public class BlackFridayTest {
 		cal.set(2021,10,29);
 	
 		double precionuevo = descuentoblackfriday.precioFinal(100.0,80.0,cal);
-		assertEquals(20.0,precionuevo);
+		assertEquals(20.0,precionuevo,0.1);
 		
 	}
 	
@@ -26,11 +26,9 @@ public class BlackFridayTest {
 		 * Caso precio original <0 siendo el día del black friday
 		 * 
 		 */
-		DescuentoBlackFriday descuentoblackfriday =new DescuentoBlackFriday();
 		cal.set(2021,10,29);
 	
 		try {
-			double precionuevo = descuentoblackfriday.precioFinal(-100.0,80.0,cal);
 			}catch (IllegalArgumentException e) {
 				return;
 			}
@@ -47,7 +45,7 @@ public class BlackFridayTest {
 		cal.set(2021,10,29);
 	
 		double precionuevo = descuentoblackfriday.precioFinal(100.0,0.0,cal);
-		assertEquals(100.0,precionuevo);
+		assertEquals(100.0,precionuevo,0.1);
 		
 	}
 	@Test
@@ -57,11 +55,9 @@ public class BlackFridayTest {
 		 * Caso descuento <0 siendo el día del black friday
 		 * 
 		 */
-		DescuentoBlackFriday descuentoblackfriday =new DescuentoBlackFriday();
 		cal.set(2021,10,29);
 	
 		try {
-			double precionuevo = descuentoblackfriday.precioFinal(100.0,-80.0,cal);
 			}catch (IllegalArgumentException e) {
 				return;
 			}
@@ -78,7 +74,7 @@ public class BlackFridayTest {
 		cal.set(2021,10,29);
 
 		double precionuevo = descuentoblackfriday.precioFinal(100.0,20.0,cal);
-		assertEquals(100.0,precionuevo);
+		assertEquals(100.0,precionuevo,0.1);
 	
 	}
 
